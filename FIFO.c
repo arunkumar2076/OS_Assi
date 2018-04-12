ee6403f303e8b52a2e2f6576878ad9f0d2e0ab10
arunkumar2076 <arunkumar2076@gmail.com>
2018-04-13 01:49:50 +0530

    random string

--- FIFO.c
+++ FIFO.c
@@ -1,10 +1,18 @@
 #include<stido.h>
 #include<conio.h>
 void main(){
-  int pages;
+  int page;
   int frames;
+	int refc_random_string[20];
+	int i;
   printf("\nEnter Total Number of Pages:\n");
 	scanf("%d", &pages);
   printf("\nEnter Total Number of Frames:\t");
 	scanf("%d", &frames);
+for(i=0; i<page; i++){
+	refc_random_string[i] = i+1;
+}
+for(i = 0; i < page; i++){
+		printf("values %d refc number %d: ", i ,refc_random_string[i]);
+}
 }
